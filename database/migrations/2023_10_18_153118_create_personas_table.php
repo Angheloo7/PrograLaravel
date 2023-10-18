c5b5988196e26231c02c83dde2d50f3d868fe731
Patty <yttap.db@gmail.com>
2023-10-18 11:51:51 -0400

    modelo y migracion realizado

---
+++ database/migrations/2023_10_18_153118_create_personas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();//llave primaria
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->integer('edad');
+            $table->integer('ci');
+            $table->text('correo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/persona/estudiante', function () {
+    //return view('welcome');
+    return "Hola desde la ruta persona estudiante";
+});
